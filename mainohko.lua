eddaaf5b2960096489767194bf90fce0b31bc46b
amzy <amzy@bullymodding.xyz>
2021-01-08 17:21:28 +0000

    fixes authority
    credit to LohaTronsRS for statement ordering idea

--- mainohko.lua
+++ mainohko.lua
@@ -1,42 +1,34 @@
---[[DAMAGE FUNCTION - Checks if Player is hit, and gives damage if returns true, else if you're hit by authority it bails out]]
 function t_damage()
-while true do
-    Wait(0)
-    if PedIsHit(gPlayer,1,500) then
-        PedApplyDamage(gPlayer,PlayerGetHealth()+1)
-    repeat
+    while true do
         Wait(0)
-    until not PedIsHit(gPlayer,1,500)
-    elseif t_auth() then
-        break
-    end
- end
-end
-
---[[DEBUG - If the player is alive and not being hit, pressed this button combo will kill you insantly / terminate script when added]]
-function t_debug()
-while true do
-    Wait(0)
-        if IsButtonPressed(10,0) and IsButtonPressed(14,0) and IsButtonPressed(8,0) then --lt leftstick b
+        if PedGetWhoHitMeLast(authTable) then
+         return
+        elseif not PedGetWhoHitMeLast(authTable) and PedIsHit(gPlayer,1,500) then
             PedApplyDamage(gPlayer,PlayerGetHealth()+1)
+        repeat
+            Wait(0)
+        until not PedIsHit(gPlayer,1,500)
         end
     end
 end
 
---[[AUTHORITY CHECK - If the player is hit and it's an authority figure, return small damage]]
-function t_auth()
-    if PedGetWhoHitMeLast(authTable) then
-        PedApplyDamage(gPlayer,1)
+--[[DEBUG - If the player is alive and not being hit, pressed this button combo will kill you insantly]]
+function t_debug()
+while true do
+    Wait(0)
+        if IsButtonPressed(10,0) and IsButtonPressed(14,0) and IsButtonPressed(8,0) then --lt leftstick b
+             PedApplyDamage(gPlayer,PlayerGetHealth()+1)
+        end
     end
 end
-
+    
 --[[THREAD CREATION]]
 function main()
-    CreateThread("t_auth")
     CreateThread("t_damage")
     CreateThread("t_debug")
 end
-
+    
+--[[TABLE]]
 authTable = {
     "49",--prefect
     "50",--prefect
