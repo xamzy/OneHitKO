f08b63f2fb7e892cbc5ed11eed14ef63bcb16f3e
amzy <amzy@bullymodding.xyz>
2021-01-08 13:02:41 +0000

    rewritten code
    wrote this on a macbook running linux with no compiler at 8pm in someone elses bedroom like an absolute champ

--- mainohko.lua
+++ mainohko.lua
@@ -1,11 +1,43 @@
-function main()
-    --CreateThread("T_falldamage") - eh; maybe.
-    CreateThread("T_damage")
-    --CreateThread("T_terminate") - if i wish to eventually toggle on and off or give the runners the ability to should the mod break mid run.
-    --CreateThread("T_grapples") - this is in regards to a bug relating to grapples. will be possibly utilised when i fix it.
+--[[DAMAGE FUNCTION - Checks if Player is hit, and gives damage if returns true, else if you're hit by authority it bails out]]
+function t_damage()
+while true do
+    Wait(0)
+    if PedIsHit(gPlayer,1,500) then
+        PedApplyDamage(gPlayer,PlayerGetHealth()+1)
+    repeat
+        Wait(0)
+    until not PedIsHit(gPlayer,1,500)
+    elseif t_auth() then
+        break
+    end
+ end
 end
 
-prefectTable = {
+--[[DEBUG - If the player is alive and not being hit, pressed this button combo will kill you insantly / terminate script when added]]
+function t_debug()
+while true do
+    Wait(0)
+        if IsButtonPressed(10,0) and IsButtonPressed(14,0) and IsButtonPressed(8,0) then --lt leftstick b
+            PedApplyDamage(gPlayer,PlayerGetHealth()+1)
+        end
+    end
+end
+
+--[[AUTHORITY CHECK - If the player is hit and it's an authority figure, return small damage]]
+function t_auth()
+    if PedGetWhoHitMeLast(authTable) then
+        PedApplyDamage(gPlayer,1)
+    end
+end
+
+--[[THREAD CREATION]]
+function main()
+    CreateThread("t_auth")
+    CreateThread("t_damage")
+    CreateThread("t_debug")
+end
+
+authTable = {
     "49",--prefect
     "50",--prefect
     "51",--prefect
@@ -35,20 +67,10 @@ prefectTable = {
     "97",--cop
     "234",--cop
     "238",--cop
+    "46", --gurney
 }
 
-function T_damage()
-TextPrintString("Welcome to OneHitKO by amzy",2,1)
-while true do
-    Wait(0)
-    if PedIsHit(gPlayer,1,500) then
-        PedApplyDamage(gPlayer,PlayerGetHealth()+1)
-        repeat
-            Wait(0)
-        until not PedIsHit(gPlayer,1,500)
-    elseif PedGetWhoHitMeLast(prefectTable) then
-        return
-        end
-    end
-    Wait(0)
-end
+--[[files used:
+linuxohko.lua
+STimeCycle.lua
+(named 1hp.lur in files)]]
